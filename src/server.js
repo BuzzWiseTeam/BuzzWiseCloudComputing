8aa4774cc65729961ee4eef6fbf3f1c74b407a13
Billy Franscois <BillyFranscois@outlook.com>
2023-06-08 00:54:47 +0900

    Update server port

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const JobRoutes = require('./routes/jobRoutes');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
