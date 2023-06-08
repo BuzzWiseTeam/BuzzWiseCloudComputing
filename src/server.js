476bb978c3726f35a054935c4e34b1b2f40ded94
Billy Franscois <BillyFranscois@outlook.com>
2023-06-09 00:02:32 +0900

    Update port and hostname

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8080;
-const hostname = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+// const hostname = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+const hostname = '0.0.0.0';
 
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
